c9af4445930b5697deb195091aa3075c1f453725
Andrea Fermo <andreafermo.sd@gmail.com>
2023-03-07 18:38:07 +0100

    esercizio concluso

--- js/main.js
+++ js/main.js
@@ -1,36 +1,59 @@
 const numberList = [];
+const enteredNumberList = [];
 const numbersToRemember = 5;
-const numbersDom = document.querySelector('#numbers')
+const numbersDom = document.querySelector('#numbers');
+const resultDom = document.querySelector('#result');
 
 for (let i = 0; i < numbersToRemember; i++) {
-    numberList.push(createUniqueRandomNumber());
+    numberList.push(createUniqueRandomNumber(numberList, 1, 10));
 }
 
 numbersDom.innerHTML = numberList;
 
-setTimeout(dNone, 5000);
+setTimeout(dNone, 30000);
 
-function createRandomNumber() {
-   return Math.floor(Math.random() * 10 + 1);
-};
+setTimeout(getNumbers, 30100);
 
 
-function createUniqueRandomNumber() {
+function createRandomNumber(min, max) {
+   return Math.floor(Math.random() * (max - min) + 1);
+}
+
+
+function createUniqueRandomNumber(list, min, max) {
     let validNumber;
     let validRandomNumber;
 
     while (!validNumber) {
-        validRandomNumber = createRandomNumber();
-        if(!numberList.includes(validRandomNumber)) {
+        validRandomNumber = createRandomNumber(min, max);
+        if(!list.includes(validRandomNumber)) {
             validNumber = !validNumber;
         }
     }
 
     return validRandomNumber;
-};
+}
 
 function dNone() {
     return numbersDom.classList.add('dNone');
+}
+
+function getNumbers() {
+    let score = 0;
+
+    for (let i = 0; i < numbersToRemember; i++) {
+        let enteredNumber = parseInt(prompt('inserisci un numero'));
+        console.log(enteredNumber);
+     
+        if (numberList.includes(enteredNumber)) {
+            enteredNumberList.push(enteredNumber)
+            console.log(`hai ricordato il numero: ${enteredNumber}`)
+            score ++;
+            console.log(score);
+        }
+    }
+
+    resultDom.innerHTML = `hai indovinato ${score} numeri e sono i seguenti: ${enteredNumberList}`;
 }
 
 
